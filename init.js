d55447d30297d96613257c8ac0c287fd8d76c0fe
waynerlc <waynerlc@gmail.com>
2014-11-14 17:06:15 -0200

    Fix no init.js

--- init.js
+++ init.js
@@ -1,4 +1,6 @@
 var chokidar = require('chokidar');
+var fs = require('fs');	
+var SerialPort = require("serialport").SerialPort;
 var monitDir = "c:\\nfce\\new";
 var watcher = chokidar.watch(monitDir, {ignored: /[\/\\]\./, persistent: true});
 
@@ -10,77 +12,21 @@ watcher
 
 var processAdd = function(path){
 
-	if(path.indexOf('.bin')>-1){
-		// Read the file and print its contents.
-		var fs = require('fs');	
+	if(path.indexOf('.bin')>-1){	
+		
 		var file = fs.readFileSync(path);		
-
-		var SerialPort = require("serialport").SerialPort;
+		fs.unlinkSync(path);
+		
 		var port = new SerialPort("COM4",{});
 		port.on('open', function(err) {
-			if(err) console.log(err);
-			//if(conteudo) console.log("conteudo = "+conteudo);
+			if(err) console.log(err);			
 			port.write(file, function(err, results) {
 				if(err)  console.log('err ' + err);
 				console.log('Done..');
 				port.close();
-				fs.unlinkSync(path);
+				
 			});
-		});
-		
-
-		/*
-		fs.open(path, 'r', function(status, fd) {
-		    if (status) {
-		        console.log(status.message);
-		        return;
-		    }
-
-		    console.log(fd.size);
-
-		    var buffer = new Buffer(fd.length);
-		    fs.read(fd, buffer, 0, buffer.length, null, function(err, num) {
-		        
-		        var SerialPort = require("serialport").SerialPort;
-				var port = new SerialPort("COM4",{});
-				port.on('open', function(err) {
-					if(err) console.log(err);
-					//if(conteudo) console.log("conteudo = "+conteudo);
-					port.write(buffer, function(err, results) {
-						if(err)  console.log('err ' + err);
-						console.log('Done..');
-						port.close();
-						fs.unlinkSync(path);
-					});
-				});
-		        //console.log(buffer.toString('utf-8', 0, num));
-		    });
-		});
-		*/		
-		
-		/*
-		fs.readFile(path, function(err, conteudo) {
-			if (err) throw err;
-			
-			//console.log('Novo arquivo: ' + path +"\n");			
-
-			console.log("Imprimindo..."+conteudo.toString());
-			var SerialPort = require("serialport").SerialPort;
-			var port = new SerialPort("COM4",{});
-
-			port.on('open', function(err) {
-				if(err) console.log(err);
-				//if(conteudo) console.log("conteudo = "+conteudo);
-				port.write(conteudo.toString()+"\r\n", function(err, results) {
-					if(err)  console.log('err ' + err);
-					console.log('Done..');
-					port.close();
-					fs.unlinkSync(path);
-				});
-			});
-
-		});
-		*/
-
+		});		
+	
 	}
 }
